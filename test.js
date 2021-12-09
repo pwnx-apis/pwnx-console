f55240bc2e42fcbc119c02de40c337b9bec168f0
Jester <76714322+Hacks52@users.noreply.github.com>
2021-12-09 22:01:42 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,15 @@
+// Console
+var $console = require('./index');
 
+// Texts
+a = 'Hello';
+b = 'World!';
+
+// Logging
+$console.log(a, b);
+$console.log('This is a LOG message');
+$console.debug('This is a DEBUG message');
+$console.error('This is a ERROR message');
+$console.warn('This is a WARN message');
+$console.stress('This is a STRESS message');
+$console.success('This is a SUCCESS message');
